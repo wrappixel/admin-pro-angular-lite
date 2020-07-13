095b87dc34ca37d96bb985f0ed4c40ac1bece2de
skchhatraliya@gmail.com <skchhatraliya@gmail.com>
2020-07-13 12:18:38 +0530

    ''

---
+++ Angular-10/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
+
+import { FullComponent } from './layouts/full/full.component';
+
+import { NavigationComponent } from './shared/header-navigation/navigation.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+import { Approutes } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SpinnerComponent } from './shared/spinner.component';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+    suppressScrollX: true,
+    wheelSpeed: 2,
+    wheelPropagation: true
+};
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        SpinnerComponent,
+        FullComponent,
+        NavigationComponent,
+        BreadcrumbComponent,
+        SidebarComponent
+    ],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        NgbModule,
+        RouterModule.forRoot(Approutes),
+        PerfectScrollbarModule,
+        ChartsModule
+    ],
+    providers: [
+        {
+            provide: PERFECT_SCROLLBAR_CONFIG,
+            useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+        }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-10/src/app/shared/sidebar/sidebar.metadata.ts
@@ -0,0 +1,11 @@
+// Sidebar route metadata
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+    label: string;
+    labelClass: string;
+    extralink: boolean;
+    submenu : RouteInfo[];
+}
