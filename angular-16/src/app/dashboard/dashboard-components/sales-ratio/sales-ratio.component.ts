2c08a2cfd4d0c2403532829f3bdaa46a80bf7971
wrappixelTeam <wrappixelteam.2016@gmail.com>
2023-05-30 19:01:17 +0530

    ''

---
+++ angular-16/src/app/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexLegend,
+  ApexXAxis,
+  ApexTooltip,
+  ApexTheme,
+  ApexGrid,
+  ApexPlotOptions,
+  ApexStroke
+
+  // ChartComponent,
+  // ApexAxisChartSeries,
+  // ApexChart,
+  // ApexXAxis,
+  // ApexDataLabels,
+  // ApexTooltip,
+  // ApexStroke
+} from 'ng-apexcharts';
+
+export type salesChartOptions = {
+  series: ApexAxisChartSeries | any;
+  chart: ApexChart | any;
+  xaxis: ApexXAxis | any;
+  yaxis: ApexYAxis | any;
+  stroke: any;
+  theme: ApexTheme | any;
+  tooltip: ApexTooltip | any;
+  dataLabels: ApexDataLabels | any;
+  legend: ApexLegend | any;
+  colors: string[] | any;
+  markers: any;
+  grid: ApexGrid | any;
+  plotOptions: ApexPlotOptions | any;
+};
+
+@Component({
+  selector: 'app-sales-ratio',
+  templateUrl: './sales-ratio.component.html'
+})
+export class SalesRatioComponent implements OnInit {
+
+  @ViewChild("chart") chart: ChartComponent = Object.create(null);
+  public salesChartOptions: Partial<salesChartOptions>;
+  constructor() {
+    this.salesChartOptions = {
+      series: [
+        {
+          name: "series1",
+          data: [31, 40, 28, 51, 42, 109, 100]
+        },
+        {
+          name: "series2",
+          data: [11, 32, 45, 32, 34, 52, 41]
+        }
+      ],
+      chart: {
+        fontFamily: 'Montserrat,sans-serif',
+        height: 290,
+        type: 'area',
+        toolbar: {
+          show: false
+        },
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ["#0d6efd", "#009efb", "#6771dc"],
+      stroke: {
+        show: true,
+        width: 4,
+        colors: ["transparent"],
+      },
+      grid: {
+        strokeDashArray: 3,
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "Aug",
+        ],
+      },
+      tooltip: {
+        theme: 'dark'
+      }
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
